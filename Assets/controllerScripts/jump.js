f6fa30144f7ba070207af4d65247df1f846e986b
Иван Данилов < >
2017-03-19 00:38:37 +0500

    add button to jump

---
+++ Assets/controllerScripts/jump.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+    if(Input.GetMouseButtonDown(0)) {
+        Debug.Log("Pressed left click.");
+    }
+    for (var i: int = 0; i < Input.touchCount; ++i) {
+	    if (Input.GetTouch(i).phase == TouchPhase.Began)
+			Debug.Log('oooooo');
+    }
+}
