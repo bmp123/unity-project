c110dd33d8e96752722c1ae447c0c51120c56f75
Иван Данилов < >
2017-03-22 21:06:57 +0500

    force ball

---
+++ Assets/controllerScripts/ball.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+private var physic: Rigidbody2D;
+private  var obj: GameObject;
+public var Mainhero: GameObject;
+private var MoveVec: Vector2;
+private var dotUp: float;
+private var flag: boolean = true;
+
+function Start () {
+    obj = gameObject;
+    physic = GetComponent.<Rigidbody2D>();
+     physic.AddForce(new Vector2(transform.position.x-Mainhero.transform.position.x,Mainhero.transform.position.y*10)*-40);
+  //  MoveVec = new Vector2(-5, Mainhero.transform.position.y + Random.Range(transform.position.y, transform.position.y + 1));
+    // physic.AddForce(transform.right * -50, ForceMode2D.Impulse);
+  
+    
+
+}
+
+function Update () {
+    
+   /* if (transform.position.x > dotUp) {
+        MoveVec.y = Mainhero.transform.position.y+5;
+    }
+   
+    if (transform.position.x <= Mainhero.transform.position.x+4) {
+        physic.velocity = Vector2(0, 0);
+    } else {
+        physic.velocity = MoveVec * 3;
+    }*/
+
+
+    
+}
+
--- Assets/controllerScripts/jump.js
+++ Assets/controllerScripts/jump.js
@@ -1,20 +1,42 @@
 ﻿#pragma strict
 
-var kolesog : GameObject;
-var koleso: float;
-
+private var balls: GameObject[];
+private var index: int = 0;
+private var indexx: int = 0;
+private var a: boolean = true;
+private var anim: Animator;
+public  var mainHero: GameObject;
+   
 function Start () {
-	
+    anim = mainHero.GetComponent.<Animator>();
 }
 
 function Update () {
-    koleso = kolesog.transform.position.y ;
-    Debug.Log('X: ' + koleso);
+    balls = GameObject.FindGameObjectsWithTag("ball");
+    index = 0;
+    for (var ball: GameObject in balls) {
+        index++;
+        if (index == balls.length) {
+            transform.position = Vector2(transform.position.x, ball.transform.position.y);
+        }
+    }
+if(a == true) {
+    indexx=0;
+    for (var ball: GameObject in GameObject.FindGameObjectsWithTag("ball")) {
+         indexx++;
+         if (indexx == balls.length) {
+           Destroy(ball, 5);
+        }
+}
+a = false;
+}
     if(Input.GetMouseButtonDown(0)) {
-        Debug.Log("Pressed left click.");
+        anim.SetBool('jump', true);
     }
-    for (var i: int = 0; i < Input.touchCount; ++i) {
-	    if (Input.GetTouch(i).phase == TouchPhase.Began)
-			Debug.Log('oooooo');
+    if(Input.GetMouseButtonUp(0)) {
+        anim.SetBool('jump', false);
     }
+    // for (var i: int = 0; i < Input.touchCount; ++i) {
+	   //  if (Input.GetTouch(i).phase == TouchPhase.Began)
+    // }
 }
